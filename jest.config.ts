d871e3b22491ec5c4a1062763fedf46726cd63f7
Takahiro <takahiro@smart-style.info>
2026-04-13 19:02:11 +0900

    jest設定修正とshadcnをdevDependenciesに移動

--- jest.config.ts
+++ jest.config.ts
@@ -3,9 +3,7 @@ import type { Config } from 'jest'
 const config: Config = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: { jsx: 'react' }
-    }]
+    '^.+\\.tsx?$': ['ts-jest', {}]
   },
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1'
