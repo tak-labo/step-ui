98d514f5afd51b9caad185d9710b5aff454cab1e
Takahiro <takahiro@smart-style.info>
2026-04-13 18:58:18 +0900

    プロジェクト初期化: Next.js + shadcn/ui + NextAuth

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: { jsx: 'react' }
+    }]
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1'
+  },
+  testMatch: ['**/__tests__/**/*.test.ts']
+}
+
+export default config
